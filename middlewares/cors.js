fc4731774433f0b6b904d4d8817642a912e93007
lital-tuli <litaltuli@gmail.com>
2025-01-14 15:48:41 +0200

    commit

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -5,6 +5,7 @@ const corsMiddleware = cors({
     "http://127.0.0.1:5500",
     "www.cardsproject.co.il",
     "http://localhost:5173",
+    "https://react-bcards-project-2.onrender.com"
   ],
 });
 
